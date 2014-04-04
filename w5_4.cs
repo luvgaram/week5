b89c4050d49562e9ad4d43d7fb50e863d0416495
임은주 <garam@nhnnext.org>
2014-04-05 00:27:29 +0900

    Create w5_4.cs

---
+++ w5_4.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace w5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Operator;
+            int firstNum;
+            int secondNum;
+            int resultNumber;
+            while (true)
+            {
+                Console.Write("Enter the operation[+,-,*,/](If you want to quit, enter 'q'): ");
+                Operator = Console.ReadLine();
+                if(Operator=="q"){
+                    Console.Write("Bye!");
+                    break;
+                }
+                else if ((Operator == "+") || (Operator == "-") || (Operator == "*") || (Operator == "/"))
+                {
+                    Console.Write("Enter the first number: ");
+                    firstNum = int.Parse(Console.ReadLine());
+                    Console.Write("Enter the second number: ");
+                    secondNum = int.Parse(Console.ReadLine());
+                    if (Operator == "+")
+                    {
+                        resultNumber = firstNum + secondNum;
+                    }
+                    else if (Operator == "-")
+                    {
+                        resultNumber = firstNum - secondNum;
+                    }
+                    else if (Operator == "*")
+                    {
+                        resultNumber = firstNum * secondNum;
+                    }
+                    else
+                    {
+                        resultNumber = firstNum / secondNum;
+                    } Console.WriteLine("Result : {0}", resultNumber);
+                }
+                else
+                {
+                    Console.WriteLine("You entered wrong operation. Only +,-,*,/,q are allowed!");
+                }
+            Console.ReadLine(); // for window7 ternimal check
+            }  
+        }
+    }
+}
