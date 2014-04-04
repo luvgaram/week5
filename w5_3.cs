53b4ebeef28cf0da811a408d1fb58c27062cfda5
임은주 <garam@nhnnext.org>
2014-04-04 20:56:00 +0900

    Update w5_3.cs

--- w5_3.cs
+++ w5_3.cs
@@ -2,34 +2,41 @@
 
 namespace w5_3
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
 
-		
-			Random random = new Random ();
-			int number = random.Next (0, 100);
-			Console.WriteLine ("AHOY! I'm the Dread Pirate Roberts, and I have a secret! It is a number from 1 to 99. I'll give you 6 tries.");
-			Console.Write ("What's Your Guess? ");
-			int guess = int.Parse (Console.ReadLine ());
-			for (int loop = 0; loop < 6; ++loop) {
-					if (guess!= number & loop == 5) {
-						Console.WriteLine ("No more Guess! Better luck next time");
-						break;
-					} else if (guess == number) {
-						Console.WriteLine ("You got it!!!");
-						break;
-					} else if (guess > number) {
-						Console.WriteLine ("Too high!");
-					} else if (guess < number) {
-						Console.WriteLine ("Too low!");
-					}
 
-				Console.Write ("What's Your Guess? ");
-				guess = int.Parse (Console.ReadLine ());
-			}
-
-		}
-	}
+            Random random = new Random();
+            int number = random.Next(0, 100);
+            int loop = 0;
+            int guess;
+            Console.WriteLine("AHOY! I'm the Dread Pirate Roberts, and I have a secret! It is a number from 1 to 99. I'll give you 6 tries.");
+            do
+            {
+                Console.Write("What's Your Guess? ");
+                guess = int.Parse(Console.ReadLine());
+                if (guess != number & loop++ == 5)
+                {
+                    Console.WriteLine("No more Guess! Better luck next time");
+                    break;
+                }
+                else if (guess == number)
+                {
+                    Console.WriteLine("You got it!!!");
+                    break;
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Too high!");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                
+            } while (true);
+        }
+    }
 }
